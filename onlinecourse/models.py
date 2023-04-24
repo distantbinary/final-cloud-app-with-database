ba0ba4f4cf1db710fc281dc796a0cc1bd389d118
distantbinary <50045675+distantbinary@users.noreply.github.com>
2023-04-24 23:53:57 +0700

    Update models.py

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -102,14 +102,9 @@ class Enrollment(models.Model):
     # Has question content
     # Other fields and methods you would like to design
 class Question(models.Model):
-    # Foreign key to course
-	course = models.ForeignKey(Course, on_delete=models.CASCADE)
-	
-    # question text
-	question_text = models.CharField(max_length=200, default="content")
-	
-    # question grade/mark
-	grade = models.IntegerField(default=0)
+    lesson_id = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+    question_text = models.TextField()
+    grade = models.IntegerField()
 
 # <HINT> A sample model method to calculate if learner get the score of the question
 def is_get_score(self, selected_ids):
@@ -128,9 +123,9 @@ def is_get_score(self, selected_ids):
     # Indicate if this choice of the question is a correct one or not
     # Other fields and methods you would like to design
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=1000)
-    is_correct = models.BooleanField(default=False)
+    choice_text = models.TextField()
+    is_correct = models.BooleanField()
+    question_id = models.ForeignKey(Question, on_delete=models.CASCADE)
 
 
 # <HINT> The submission model
